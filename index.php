4a1f83d5ae219f7ae2ddca17a60f4e212ad51bf1
YY <4841601@gmail.com>
2018-03-09 14:28:51 +0200

    start

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+@error_reporting(E_ALL);
+@ini_set('display_errors', true);
+@ini_set('html_errors', true);
+@ini_set('error_reporting', E_ALL);
+
+date_default_timezone_set('Europe/Kiev');
+echo '"yytest" on ' . date('r');
+
